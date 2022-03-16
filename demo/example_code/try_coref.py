167b719c917bac2eb57c8aad8ee462d34f664cfc
涂尚卿 <1103435855@qq.com>
2022-03-16 22:17:40 +0800

    try chinese co-reference

--- demo/example_code/try_coref.py
+++ demo/example_code/try_coref.py
@@ -5,7 +5,8 @@ from stanfordnlp.server import CoreNLPClient
 print('---')
 print('input text')
 print('')
-text = "Chris Manning is a nice person. Chris wrote a simple sentence. He also gives oranges to people."
+# text = "Chris Manning is a nice person. Chris wrote a simple sentence. He also gives oranges to people."
+text = "习近平出生在中国。 他是中国的总统."
 print(text)
 
 # set up the client
@@ -13,11 +14,24 @@ print('---')
 print('starting up Java Stanford CoreNLP Server...')
 
 # set up the client
-with CoreNLPClient(annotators=['tokenize', 'ssplit', 'coref'],
-                   timeout=30000, memory='16G') as client:
-    # submit the request to the server
+with CoreNLPClient(
+        properties="chinese",
+        annotators=['coref'],
+        endpoint="http://localhost:5414",
+        timeout=30000, memory='16G') as client:
     ann = client.annotate(text)
 
+    #         properties={
+    #             "annotators": "tokenize,ssplit,coref",
+    #             'tokenize.language': 'zh',
+    #
+    #         },
+    #         endpoint="http://localhost:5414",
+    #         timeout=30000, memory='16G', output_format='json') as client:
+    #     # submit the request to the server
+    #     ann = client.annotate(text, output_format='json')
+    # client = CoreNLPClient(start_server=False)
+    # ann = client.annotate(text=text, properties={"inputFormat": "text", "outputFormat": "json"})
     # sentence and token
     for sentence in ann.sentence:
         for token in sentence.token:
@@ -26,4 +40,5 @@ with CoreNLPClient(annotators=['tokenize', 'ssplit', 'coref'],
     # access the coref chain
     print('---')
     print('coref chains for the example')
-    print(ann.corefChain.getRepresentativeMention())
+    print(ann.corefChain)
+    # print(ann)
